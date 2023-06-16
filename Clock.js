8f62b22a3c5414572381596cd109753c804d3779
SANTHEESH.S <136729279+Santheesh092@users.noreply.github.com>
2023-06-16 07:04:55 +0530

    Update Clock.htmljs

--- Clock.js
+++ Clock.js
@@ -8,7 +8,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">	<title>Working Clock</title>
 
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="Clock.css">
 
 </head>
 
